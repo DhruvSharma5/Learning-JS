8976c36b40ed1fb6b514017c8a8dd7e0e4cea417
DhruvSharma5 <21cse063@glbitm.ac.in>
2026-08-24 16:10:06 +0530

    initial commit

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3); we are using node js not browser
+
+console.log(3+3)
+// code readability that should be high
+ console.log("Dhruv")
+
+ let name = "Dhruv"
+ let age = 21
+ let isLoggedIn = false
+ let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean=> true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+
+// object
+console.log(typeof age);
